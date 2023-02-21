29fd6ba2a44941e32c786ac1db18429355fde84d
thiaaagao <rdes.thiago@gmail.com>
2023-02-21 05:10:23 -0300

    Add var name

--- cookbook.rb
+++ cookbook.rb
@@ -1,1 +1,9 @@
 # Playlist Lógica de programação com Ruby - Livro de receitas 
+
+puts "Bem-vindo ao Cookbook, sua rede social de receitas"
+
+puts "Digite O nome da receita:"
+
+nome = gets.chomp
+
+puts ("Receita cadastrada #{nome} com sucesso!")
